20eabc3e30127b09fc841b30beac2ddd80bcea3e
enniowolsinkRGN <96447443+enniowolsinkRGN@users.noreply.github.com>
2023-11-02 15:42:37 +0100

    Update index.js
    This is a test

--- bin/file-parsers/markdown-file-parser/index.js
+++ bin/file-parsers/markdown-file-parser/index.js
@@ -23,6 +23,7 @@ module.exports = class MarkdownFileParser {
         if (!(file.endsWith('.md') && !file.endsWith('.message.md') && !file.endsWith('.email.md')))
             return;
 
+        console.info('This is a test');
         console.info(colors.yellow(`parsing ${path.relative(this.options.dst, file)}`));
 
         const htmlFile = `${file.slice(0, -3)}.html`;
